df54028f2ceac8ae90dfead9a5caf025a4005775
Alencruser <alencruserxx@hotmail.fr>
2024-09-20 14:17:45 +0200

    add nestjs config tu use .env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [
+        ConfigModule.forRoot(),
         TypeOrmModule.forRoot({
-            type: 'mysql'
-        })
+            type: 'mysql',
+        }),
     ],
     controllers: [AppController],
-    providers: [AppService]
+    providers: [AppService],
 })
 export class AppModule {}
