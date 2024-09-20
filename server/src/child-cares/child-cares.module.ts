36822b387af78438b1e31de453b8fcfd575e3709
Alencruser <alencruserxx@hotmail.fr>
2024-09-20 17:44:30 +0200

    created a childCare child service to handle the many to many between children and child cares

--- server/src/child-cares/child-cares.module.ts
+++ server/src/child-cares/child-cares.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChildCaresService } from './child-cares.service';
 import { ChildCaresController } from './child-cares.controller';
+import { ChildCareChildService } from 'src/child_care_child/child_care_child.service';
 
 @Module({
-  controllers: [ChildCaresController],
-  providers: [ChildCaresService],
+    controllers: [ChildCaresController],
+    providers: [ChildCaresService, ChildCareChildService],
 })
 export class ChildCaresModule {}
