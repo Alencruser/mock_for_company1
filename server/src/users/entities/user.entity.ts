b7bbd264fc2adc9a580ad48da3a670920bcde709
Alencruser <alencruserxx@hotmail.fr>
2024-09-21 19:05:08 +0200

    prepared the user endpoints

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -1,4 +1,10 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity()
-export class User {}
+export class User {
+    @PrimaryColumn('varchar', { length: 255, unique: true })
+    email: string;
+
+    @Column('varchar', { length: 50, unique: true })
+    username: string;
+}
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,34 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Get, Put, Query } from '@nestjs/common';
+import { CreateAndUpdateDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('users')
+@Controller('user')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
+    @Get()
+    find(@Query('username') username: string) {
+        return this.usersService.findOne(username);
+    }
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
-  }
+    @Put()
+    createOrUpdate(@Body() createAndUpdateDto: CreateAndUpdateDto) {
+        return this.usersService.createOrUpdate(createAndUpdateDto);
+    }
 }
