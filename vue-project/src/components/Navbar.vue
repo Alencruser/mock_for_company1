792bcd0f91b7a02a7f86c4457ab9e50da3acbf5a
Alencruser <alencruserxx@hotmail.fr>
2024-09-22 17:26:58 +0200

    config of the front for the momen

---
+++ vue-project/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { BCol, BContainer, BNav, BNavItem, BRow } from 'bootstrap-vue-next';
+const username = localStorage.getItem('username');
+const amIConnected = username?.length;
+</script>
+
+<template>
+    <BContainer v-if="amIConnected">
+        <BRow>
+            <BCol alignSelf="center">
+                <BNav tabs>
+                    <BNavItem active>Active</BNavItem>
+                    <BNavItem>Link</BNavItem>
+                    <BNavItem>Another Link</BNavItem>
+                    <BNavItem disabled>Bonjour {{ username }}</BNavItem>
+                </BNav>
+            </BCol>
+        </BRow>
+    </BContainer>
+</template>
+
+<style scoped>
+#navbar {
+    position: fixed;
+    top: 0;
+}
+</style>
