6d01ce5bb35d96649c539fb8ea1affdbbf64a635
Alencruser <alencruserxx@hotmail.fr>
2024-09-22 15:27:45 +0200

    fixed migration for child_carE_child

--- server/migrations/1726842414251-childCare_child.ts
+++ server/migrations/1726842414251-childCare_child.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class ChildCareChild1726842414251 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `CREATE TABLE child_care_child (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, id_child_care INT NOT NULL, id_child INT NOT NULL, FOREIGN KEY(id_child_care) REFERENCES child_care(id), FOREIGN KEY(id_child) REFERENCES child(id))`,
+            `CREATE TABLE child_care_child (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, child_care_id INT NOT NULL, child_id INT NOT NULL, FOREIGN KEY(child_care_id) REFERENCES child_care(id), FOREIGN KEY(child_id) REFERENCES child(id))`,
         );
     }
 
