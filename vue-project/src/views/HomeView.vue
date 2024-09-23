666ff7f7b0e56657f19f5637dc92577ef0d7bc56
Alencruser <alencruserxx@hotmail.fr>
2024-09-23 23:28:46 +0200

    made a listener on my homeView that send me back to login page if i loose the credentials

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
 import Navbar from '@/components/Navbar.vue';
+import { authService } from '@/services/auth';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+window.addEventListener('storage', () => {
+    if (!authService.isConnected()) router.push({ name: 'login' });
+});
 </script>
 
 <template>
