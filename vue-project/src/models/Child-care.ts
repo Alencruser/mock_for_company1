c52705f84a5eb0e5f891b8f95d741beef786d1cf
Alencruser <alencruserxx@hotmail.fr>
2024-09-23 23:33:57 +0200

    added models

---
+++ vue-project/src/models/Child-care.ts
@@ -0,0 +1,5 @@
+export type ChildCare = {
+    id: number;
+    name: string;
+    referent: string;
+};
---
+++ vue-project/src/models/Child.ts
@@ -0,0 +1,6 @@
+export type Child = {
+    id: number;
+    first_name: string;
+    last_name: string;
+    referent: string;
+};
