273e371832eb0070d0c872ccbf92ec0a65e41a03
Alencruser <alencruserxx@hotmail.fr>
2024-09-23 23:36:59 +0200

    change type in login and register function to return User Error or null according to the back

--- vue-project/src/services/auth.ts
+++ vue-project/src/services/auth.ts
@@ -1,12 +1,13 @@
+import type { User } from '@/models/User';
 import { customFetch } from '@/utils/customFetch';
 
 export const authService = {
-    async login(username: string): Promise<string | Error> {
+    async login(username: string): Promise<User | Error | null> {
         const response = await customFetch(`user?username=${username}`);
         if (response?.username?.length) this.storeUser(response.username);
         return response;
     },
-    async register(username: string, email: string): Promise<string | Error> {
+    async register(username: string, email: string): Promise<User | Error | null> {
         const response = await customFetch('user', { username, email }, { method: 'PUT' });
         if (response?.username?.length) this.storeUser(response.username);
         return response;
